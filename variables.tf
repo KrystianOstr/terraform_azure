eb8e225d7fdb3f1887f3cab8ba2a3025137fb9fb
Krystian <krystian.ostrogorski@gmail.com>
2025-08-28 10:07:49 +0200

    rg with variables and output

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+  type        = string
+  description = "Name of the RG"
+  default     = "my_main_rg"
+}
+
+variable "rg_location" {
+  type        = string
+  description = "Name of the Location for resources"
+  default     = "East US"
+}
